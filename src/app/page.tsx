2a59fcf593115c41e4410cefe6e8444824107571
ldg-96 <leedonggwon1996@gmail.com>
2025-05-30 21:29:14 +0900

    yml 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@
 import AuthGuard from '@/app/_components/authGuard'
 
 export default function Home() {
-  return <AuthGuard>임시 / 경로 페이지입니다.</AuthGuard>
+  return <AuthGuard>임시/경로 보이지않을예정</AuthGuard>
 }
