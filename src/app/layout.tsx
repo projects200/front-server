343eb49aac858592a8351b99d79cbaf1cd67aaf9
ldg-96 <leedonggwon1996@gmail.com>
2025-06-20 02:05:41 +0900

    (Chore) csp 정책 nonce 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Script from 'next/script'
 import type { Metadata } from 'next'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 // import { GoogleTagManager } from '@next/third-parties/google'
