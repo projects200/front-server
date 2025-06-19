0e8f361f776a821e83384c7b28b0b90b3014734e
ldg-96 <leedonggwon1996@gmail.com>
2025-06-20 03:24:37 +0900

    (Chore) csp 정책 nonce 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
-import { GoogleTagManager } from '@next/third-parties/google'
+// import { GoogleTagManager } from '@next/third-parties/google'
 
 import { ClientProviders } from './_components/clientProviders'
 
 import './reset.css'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: '운다방',
@@ -21,10 +22,36 @@ export default function RootLayout({
   const gtmId = process.env.NEXT_PUBLIC_GTM_ID
 
   return (
-    <html lang="ko" nonce={nonce}>
-      <head nonce={nonce}></head>
-      {gtmId && <GoogleTagManager gtmId={gtmId} />}
-      <body nonce={nonce}>
+    <html lang="ko">
+      <head>
+        {gtmId && (
+          <Script
+            id="gtm-base"
+            strategy="afterInteractive"
+            nonce={nonce}
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${gtmId}');
+              `,
+            }}
+          />
+        )}
+      </head>
+      <body>
+        {gtmId && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+        )}
         <ClientProviders>
           <NuqsAdapter>
             <div className="responsive-container">{children}</div>
