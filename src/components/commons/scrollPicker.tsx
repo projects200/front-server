9ae50f651fc0894e6b5e79428e30a8decf1dcb23
ldg-96 <leedonggwon1996@gmail.com>
2025-09-02 06:45:51 +0900

    (not issue)

--- src/components/commons/scrollPicker.tsx
+++ src/components/commons/scrollPicker.tsx
@@ -72,10 +72,19 @@ export default function ScrollPicker({
   }
 
   useEffect(() => {
+    const scrollY = window.scrollY
+
+    document.body.style.position = 'fixed'
+    document.body.style.width = '100%'
+    document.body.style.top = `-${scrollY}px`
     document.body.style.overflow = 'hidden'
 
     return () => {
-      document.body.style.overflow = 'unset'
+      document.body.style.position = ''
+      document.body.style.width = ''
+      document.body.style.top = ''
+      document.body.style.overflow = ''
+      window.scrollTo(0, scrollY)
     }
   }, [])
 
