89346821b751b6dc0ce08e13e6ddbb4749481b2c
ldg-96 <leedonggwon1996@gmail.com>
2025-09-05 00:27:04 +0900

    (not issue)

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,7 +3,6 @@ import { fetchWrapper } from '@/utils/fetchWrapper'
 
 // 유저 생성
 export function createUser(token: string, data: SignUp): Promise<MemberInfo> {
-  console.log(data)
   return fetchWrapper<MemberInfo>(
     `${process.env.NEXT_PUBLIC_API_DOMAIN}/auth/v1/sign-up`,
     {
@@ -19,11 +18,12 @@ export function createUser(token: string, data: SignUp): Promise<MemberInfo> {
 }
 
 // 유저 회원가입 여부 확인
-export function readRegistered(token: string): Promise<{ isRegistered: boolean }> {
+export function readRegistered(
+  token: string,
+): Promise<{ isRegistered: boolean }> {
   return fetchWrapper<{ isRegistered: boolean }>(
     `${process.env.NEXT_PUBLIC_API_DOMAIN}/auth/v1/registration-status`,
     { method: 'GET' },
     token,
   )
 }
-
