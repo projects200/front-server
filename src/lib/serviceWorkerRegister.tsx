2cc7430a98898791696ad9edce96ce134b0bc798
ldg-96 <leedonggwon1996@gmail.com>
2025-09-17 02:36:40 +0900

    (bug)SW 무한 리로딩 문재 해결

--- src/lib/serviceWorkerRegister.tsx
+++ src/lib/serviceWorkerRegister.tsx
@@ -4,41 +4,39 @@ import { useEffect } from 'react'
 
 export default function ServiceWorkerRegister() {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then((registration) => {
-          console.log('Service Worker 등록 완료:')
-          registration.addEventListener('updatefound', () => {
-            const newWorker = registration.installing
-            console.log('새로운 서비스 워커 발견:')
+    if (!('serviceWorker' in navigator)) {
+      return
+    }
 
-            if (newWorker) {
-              newWorker.addEventListener('statechange', () => {
-                if (
-                  newWorker.state === 'installed' &&
-                  navigator.serviceWorker.controller
-                ) {
-                  console.log('새로운 워커 설치 완료. 활성화를 시작합니다.')
-                  newWorker.postMessage({ type: 'SKIP_WAITING' })
-                }
-              })
+    const registerServiceWorker = async () => {
+      try {
+        const registration = await navigator.serviceWorker.register('/sw.js')
+        console.log('Service Worker 등록 완료')
+
+        registration.addEventListener('updatefound', () => {
+          const newWorker = registration.installing
+          if (!newWorker) return
+          
+          console.log('새로운 서비스 워커 발견')
+
+          newWorker.addEventListener('statechange', () => {
+            if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+              console.log('새로운 워커 설치 완료. 활성화를 시작합니다.')
+              navigator.serviceWorker.addEventListener('controllerchange', () => {
+                console.log('새로운 버전 활성화 완료. 페이지를 새로고침합니다.')
+                window.location.reload()
+              }, { once: true });
+
+              newWorker.postMessage({ type: 'SKIP_WAITING' })
             }
           })
         })
-        .catch((err) => {
-          console.error('Service Worker 등록 실패:', err)
-        })
-
-      let refreshing = false
-      navigator.serviceWorker.addEventListener('controllerchange', () => {
-        if (!refreshing) {
-          console.log('새로운 버전 활성화 완료. 페이지를 새로고침합니다.')
-          window.location.reload()
-          refreshing = true
-        }
-      })
+      } catch (err) {
+        console.error('Service Worker 등록 실패:', err)
+      }
     }
+
+    registerServiceWorker()
   }, [])
 
   return null
