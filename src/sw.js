869b7deb5377ea0f61494108b225f91e3dab8e11
ldg-96 <leedonggwon1996@gmail.com>
2025-09-21 03:59:17 +0900

    (#164) 카카오 지도 API 연결 및 화면에 표시

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js',
-)
+// importScripts(
+//   'https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js',
+// )
 importScripts('/firebase-messaging-sw.js')
 
 self.addEventListener('install', () => {
@@ -13,21 +13,21 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim())
 })
 
-if (workbox) {
-  console.log(`Workbox is loaded. Applying runtime caching.`)
+// if (workbox) {
+//   console.log(`Workbox is loaded. Applying runtime caching.`)
 
-  const staleWhileRevalidate = new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'static-resources',
-  })
+//   const staleWhileRevalidate = new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: 'static-resources',
+//   })
 
-  // JS, CSS, Web Font 파일에 대해 캐싱 전략을 적용합니다.
-  workbox.routing.registerRoute(
-    ({ request }) =>
-      request.destination === 'script' ||
-      request.destination === 'style' ||
-      request.destination === 'font',
-    staleWhileRevalidate,
-  )
-} else {
-  console.log(`Workbox didn't load.`)
-}
+//   // JS, CSS, Web Font 파일에 대해 캐싱 전략을 적용합니다.
+//   workbox.routing.registerRoute(
+//     ({ request }) =>
+//       request.destination === 'script' ||
+//       request.destination === 'style' ||
+//       request.destination === 'font',
+//     staleWhileRevalidate,
+//   )
+// } else {
+//   console.log(`Workbox didn't load.`)
+// }
