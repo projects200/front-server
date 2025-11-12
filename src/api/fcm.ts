11ffc1b1070741f2767cc8a0d5a7bd98dde1c643
ldg-96 <leedonggwon1996@gmail.com>
2025-11-13 04:01:58 +0900

    (#218) 알림 기능 API 연결

--- src/api/fcm.ts
+++ src/api/fcm.ts
@@ -1,3 +1,6 @@
+import { NotificationSettingItems, NotificationSetting } from '@/types/fcm'
+import { NotificationSettingDto } from '@/types/dto/fcm.dto'
+import { adapterNotificationSettingListToDtoList } from '@/lib/adapters/fcm.adapter'
 import { fetchWrapper } from '@/utils/fetchWrapper'
 
 // FCM 토큰 전달
@@ -23,6 +26,44 @@ export function removeFcmToken(token: string, fcmToken: string): Promise<null> {
       headers: {
         'X-Fcm-Token': fcmToken,
       },
+    },
+    token,
+  )
+}
+
+// 특정 기기 알림 상태 조회
+export function readNotificationSettingList(
+  token: string,
+  fcmToken: string | null,
+): Promise<NotificationSetting[]> {
+  return fetchWrapper<NotificationSetting[]>(
+    `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/v1/notification-settings/device`,
+    {
+      method: 'GET',
+      headers: {
+        ...(fcmToken && { 'X-Fcm-Token': fcmToken }),
+      },
+    },
+    token,
+  )
+}
+
+// 알림 상태 수정(on/off)
+export function updateNotificationSettingItems(
+  token: string,
+  fcmToken: string | null,
+  data: NotificationSetting[],
+): Promise<NotificationSettingItems> {
+  const dotList: NotificationSettingDto[] =
+    adapterNotificationSettingListToDtoList(data)
+  return fetchWrapper<NotificationSettingItems>(
+    `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/v1/notification-settings/device`,
+    {
+      method: 'PATCH',
+      headers: {
+        ...(fcmToken && { 'X-Fcm-Token': fcmToken }),
+      },
+      body: JSON.stringify(dotList),
     },
     token,
   )
