75ec2a79315e69c74308e5d46cca4d9deceeb0b7
ldg-96 <leedonggwon1996@gmail.com>
2025-11-28 01:22:15 +0900

    (#231) 채팅메시지 보낼 때 GA 이벤트 생성

--- src/lib/firebase/analytics.tsx
+++ src/lib/firebase/analytics.tsx
@@ -4,15 +4,31 @@ import { useEffect } from 'react'
 import { getAnalytics, isSupported, logEvent } from 'firebase/analytics'
 import { firebaseApp } from './config'
 
+type AnalyticsParams = {
+  [key: string]: string | number | boolean | undefined
+}
+
+export const logAnalyticsEvent = async (
+  eventName: string,
+  params?: AnalyticsParams,
+) => {
+  if (typeof window === 'undefined') return
+
+  try {
+    const supported = await isSupported()
+    if (supported) {
+      const analytics = getAnalytics(firebaseApp)
+      logEvent(analytics, eventName, params)
+    }
+  } catch (error) {
+    console.error('구글 애널리틱스 로그 에러 : ', error)
+  }
+}
+
 function Analytics() {
   useEffect(() => {
-    isSupported().then((supported) => {
-      if (supported) {
-        const analytics = getAnalytics(firebaseApp)
-        logEvent(analytics, 'page_view', {
-          page_path: window.location.pathname,
-        })
-      }
+    logAnalyticsEvent('page_view', {
+      page_path: window.location.pathname,
     })
   }, [])
   return null
