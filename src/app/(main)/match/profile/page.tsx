7d009839f6a258af6e5b7680abcfaa73ab954649
ldg-96 <leedonggwon1996@gmail.com>
2026-01-09 20:09:47 +0900

    (#237) 선호운동 API 연결

--- src/app/(main)/match/profile/page.tsx
+++ src/app/(main)/match/profile/page.tsx
@@ -25,25 +25,6 @@ import styles from './profile.module.css'
 
 import { useRemoteConfig } from '@/hooks/useRemoteConfig' //12월 17일 제거
 
-const TEMP_DATA = [
-  {
-    preferredExerciseId: 1,
-    exerciseTypeId: 1,
-    name: '테니스',
-    skillLevel: 'BEGINNER',
-    daysOfWeek: [true, true, true, true, true, true, true],
-    imageUrl: null,
-  },
-  {
-    preferredExerciseId: 2,
-    exerciseTypeId: 2,
-    name: '축구',
-    skillLevel: 'BEGINNER',
-    daysOfWeek: [true, false, true, true, false, true, true],
-    imageUrl: null,
-  },
-]
-
 export default function Profile() {
   const { config, isLoading: remoteConfigIsLoading } = useRemoteConfig() //12월17일 제거
 
@@ -186,7 +167,7 @@ export default function Profile() {
 
       {/* 선호운동 영역 */}
       {/* 12월 17일 플래그 제거 */}
-      {config.new_feautre_flag && TEMP_DATA.length > 0 && (
+      {config.new_feautre_flag && profileData.preferredExercises.length > 0 && (
         <section className={styles['prefer-exercise-section']}>
           <div className={styles['prefer-exercise-title']}>
             <Typography as="p" variant="content-large" weight="bold">
@@ -194,7 +175,7 @@ export default function Profile() {
             </Typography>
           </div>
           <div className={styles['prefer-exercise-list']}>
-            {TEMP_DATA.map((data) => (
+            {profileData.preferredExercises.map((data) => (
               <PreferExerciseItem
                 key={`prefer-${data.preferredExerciseId}`}
                 data={data}
